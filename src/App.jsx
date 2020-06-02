9cd7ecc8ff0a71aeeac6f94a1476761c94208d47
fangfelipe <fangfelipe@gmail.com>
2020-06-02 12:14:14 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>55555555555</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default App
+
+  
